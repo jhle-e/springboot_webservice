3659b9923983e4b6fca6dc9951b2f07a56f397f5
easy-h <jhlee7129@gmail.com>
2020-01-21 22:48:33 +0900

    responseDto 테스트코드 추가

--- src/main/java/com/easyh/book/springboot/web/HelloController.java
+++ src/main/java/com/easyh/book/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.easyh.book.springboot.web;
 
+import com.easyh.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 //JSON 반환하는 컨트롤러 (@ResponseBody 역할)
@@ -10,5 +12,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
     }
 }
