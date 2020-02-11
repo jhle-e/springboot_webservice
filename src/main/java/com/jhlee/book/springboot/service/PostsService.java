064f7b5e7df801352b3771aace28d6558c482bce
jhle-e <jhlee7129@gmail.com>
2020-02-11 22:38:23 +0900

    service, dto, controller 추가 및 어노테이션 학습

---
+++ src/main/java/com/jhlee/book/springboot/service/PostsService.java
@@ -0,0 +1,19 @@
+package com.jhlee.book.springboot.service;
+
+import com.jhlee.book.springboot.domain.posts.PostsRepository;
+import com.jhlee.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
