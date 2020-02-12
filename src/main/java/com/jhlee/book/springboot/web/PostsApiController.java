97e0526b1425f46785bacfe8a56ee17629116ea0
jhle-e <jhlee7129@gmail.com>
2020-02-12 23:47:56 +0900

    2020_0212
    - Posts 테스트 코드 추가
    - Controller Annotation PutMapping -> PostMapping 수정
    - 테스트 properties 추가 (spring.h2.console.enabled=true 옵션 추가)

--- src/main/java/com/jhlee/book/springboot/web/PostsApiController.java
+++ src/main/java/com/jhlee/book/springboot/web/PostsApiController.java
@@ -3,7 +3,7 @@ package com.jhlee.book.springboot.web;
 import com.jhlee.book.springboot.service.PostsService;
 import com.jhlee.book.springboot.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +13,7 @@ public class PostsApiController {
 
     private final PostsService postsService;
 
-    @PutMapping("/api/v1/posts")
+    @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
     }
