d3d77966611e1eb7d8cdd31ab64f1eb70f736be3
jhle-e <jhlee7129@gmail.com>
2020-02-24 23:49:05 +0900

    스프링 부트 버전 업 및 OAuth2 로그인 버그 수정

--- src/main/java/com/jhlee/book/springboot/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/jhlee/book/springboot/config/auth/CustomOAuth2UserService.java
@@ -4,6 +4,8 @@ import com.jhlee.book.springboot.config.auth.dto.OAuthAttributes;
 import com.jhlee.book.springboot.config.auth.dto.SessionUser;
 import com.jhlee.book.springboot.domain.user.User;
 import com.jhlee.book.springboot.domain.user.UserRepository;
+
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
@@ -20,7 +22,6 @@ import java.util.Collections;
 @RequiredArgsConstructor
 @Service
 public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
-
     private final UserRepository userRepository;
     private final HttpSession httpSession;
 
@@ -30,28 +31,27 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
         OAuth2User oAuth2User = delegate.loadUser(userRequest);
 
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
-
         String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails()
-                                        .getUserInfoEndpoint().getUserNameAttributeName();
+                .getUserInfoEndpoint().getUserNameAttributeName();
 
         OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
 
         User user = saveOrUpdate(attributes);
-
         httpSession.setAttribute("user", new SessionUser(user));
 
         return new DefaultOAuth2User(
                 Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey())),
                 attributes.getAttributes(),
-                attributes.getNameAttributeKey()
-        );
+                attributes.getNameAttributeKey());
     }
+
 
     private User saveOrUpdate(OAuthAttributes attributes) {
         User user = userRepository.findByEmail(attributes.getEmail())
-                    .map(entity -> entity.update(attributes.getName(), attributes.getPicture()))
-                    .orElse(attributes.toEntity());
+                .map(entity -> entity.update(attributes.getName(), attributes.getPicture()))
+                .orElse(attributes.toEntity());
 
         return userRepository.save(user);
     }
 }
+
--- src/main/java/com/jhlee/book/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/jhlee/book/springboot/config/auth/SecurityConfig.java
@@ -13,19 +13,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final CustomOAuth2UserService customOAuth2UserService;
 
     @Override
-    protected  void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
                 .headers()
                 .frameOptions()
                 .disable()
                 .and()
                     .authorizeRequests()
-                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**")
-                    .hasRole(Role.USER.name())
-                    .anyRequest().authenticated()
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                    .antMatchers("/api/v1/**")
+                        .hasRole(Role.USER.name())
+                        .anyRequest().authenticated()
                 .and()
                     .logout()
-                        .logoutSuccessUrl("/")
+                    .logoutSuccessUrl("/")
                 .and()
                     .oauth2Login()
                         .userInfoEndpoint()
@@ -33,3 +35,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
 }
+
